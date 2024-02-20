44b3eb8a634bc2b8c004875d55e221fa116d27e5
Navili134 <nmaladina@hotmail.com>
2024-02-20 20:21:18 +1300

    initiated global.css styling on Nav.tsx

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <nav className="w-full relative flex items-center justify-between max-w-3xl mx-auto">
+      <h1>child1</h1>
+      <h1>child2</h1>
+    </nav>
+  );
+}
